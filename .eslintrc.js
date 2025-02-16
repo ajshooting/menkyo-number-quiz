643dd58ea5813b71e172c4ce646addd2980ad128
ajshooting <ajshooting.aj@gmail.com>
2025-02-17 00:08:14 +0900

    nuxt2 -> nuxt3 with 4o

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     browser: true,
     node: true
   },
+  parser: 'vue-eslint-parser',
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,48 +1,50 @@
-export default {
-  // Target: https://go.nuxtjs.dev/config-target
+export default defineNuxtConfig({
+  // Target: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'menkyo-number-quiz',
-    htmlAttrs: {
-      lang: 'ja'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+  // Global page headers: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#head
+  app: {
+    head: {
+      title: 'menkyo-number-quiz',
+      htmlAttrs: {
+        lang: 'ja'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  // Global CSS: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#css
   css: [
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  // Plugins to run before rendering page: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  // Modules for dev and build (recommended): https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#buildmodules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
+    // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
+    // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
+  // Modules: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#modules
   modules: [
   ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  // Build Configuration: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#build
   build: {
     // postcss の設定は削除 (@nuxtjs/tailwindcss が自動で設定)
     extend(config, { isDev, isClient }) {
@@ -60,4 +62,4 @@ export default {
   router: {
     base: '/menkyo-number-quiz/'
   }
-}
+})
