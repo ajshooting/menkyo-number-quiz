f71adba9b1dbf513ccd7ca5105803457ad875fa9
ajshooting <ajshooting.aj@gmail.com>
2025-02-20 23:48:42 +0900

    buttonのcssを修正

--- components/button.vue
+++ components/button.vue
@@ -1,9 +1,8 @@
 <template>
   <button :style="{ backgroundColor: color }" @click="$router.push({ path: '/quiz', query: { level } })"
-    class="block mx-auto my-4 py-2 px-4 rounded-lg text-white">
-    {{ text }}
-    <br>
-    <small>{{ description }}</small>
+    class="block min-w-52 mx-auto my-4 py-2 px-4 rounded-lg text-white">
+    <div class="font-bold py-1">{{ text }}</div>
+    <div v-show="description" class="text-xs my-1">{{ description }}</div>
   </button>
 </template>
 
