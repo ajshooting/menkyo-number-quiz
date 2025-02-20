c45d324a974dad304257e6c9780558c15499cfa1
ajshooting <ajshooting.aj@gmail.com>
2025-02-20 23:48:24 +0900

    index.vueをいい感じに

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h1>クイズ！</h1>
-    <button @click="$router.push('/quiz')">
-      クイズ開始
-    </button>
+    <Button text="難易度 1" description="基本的な内容です" color="#4A90E2" level="1" />
+    <Button text="難易度 2" description="学科試験には多分出ません" color="#357ABD" level="2" />
+    <Button text="難易度 3" description="知るわけがないです" color="#2A5D99" level="3" />
+    <Button text="難易度 4" description="流石にひどいです" color="#1E3F75" level="4" />
+    <br>
+    <Button text="全問題" description="" color="#666666" level="0" />
   </div>
 </template>
