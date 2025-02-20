70a4bd0a1c09dfb5c801ccee83bf3aacf2a02068
ajshooting <ajshooting.aj@gmail.com>
2025-02-20 22:32:20 +0900

    buttonコンポーネントを作成

---
+++ components/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :style="{ backgroundColor: color }" @click="$router.push({ path: '/quiz', query: { level } })"
+    class="block mx-auto my-4 py-2 px-4 rounded-lg text-white">
+    {{ text }}
+    <br>
+    <small>{{ description }}</small>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
