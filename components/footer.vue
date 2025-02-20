1268a4e65352f4288590ecedb156983341e8a7d3
ajshooting <ajshooting.aj@gmail.com>
2025-02-20 22:32:42 +0900

    footerをすてぃっきーに

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="fixed bottom-0 w-full bg-gray-100 text-center py-2">
+  <footer class="sticky bottom-0 w-full bg-gray-100 text-center py-2">
     <div class="footer-content">
       <a href="https://twitter.com/ajshooting" target="_blank" class="mx-2 text-gray-800 hover:underline">Twitter</a> |
       <a href="https://github.com/ajshooting/menkyo-number-quiz" target="_blank"
