ddb7917cb39cb529f506192af286c186c6c99c12
ajshooting <ajshooting.aj@gmail.com>
2025-03-02 23:33:33 +0900

    headerをtopへリンク

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="bg-gray-100 text-center py-3">
-    <h1 class="text-xl font-bold">menkyo-number-quiz</h1>
+    <h1 class="text-xl font-bold" @click="$router.push('/')">menkyo-number-quiz</h1>
   </header>
 </template>
